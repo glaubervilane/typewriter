c344fdc70f56f86487ab228f03b6caf1b64d2e6c
Glauber Marques <glauberfmarques@gmail.com>
2023-05-12 06:35:43 -0600

    Implementing an animated sentence to output characters using setTimeout

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
+
+setTimeout(() => {
+  console.log(); // Output a newline character
+}, delay);
